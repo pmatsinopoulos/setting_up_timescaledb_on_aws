3d4063ce17a4bde21268d0eae0f4ea34102d5a11
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2025-07-10 19:19:27 +0300

    Intallation of 16 is ok

--- terraform/install_postgres.sh
+++ terraform/install_postgres.sh
@@ -23,35 +23,45 @@ sudo DEBIAN_FRONTEND=noninteractive /usr/share/postgresql-common/pgdg/apt.postgr
 # Let's now install on top of any existing
 CURRENT_CLUSTERS=$(sudo pg_lsclusters -h 2>/dev/null | grep "${PORT}" | awk '{print $1"-"$2}' || true)
 
+INSTALL="yes"
 if [ -n "$CURRENT_CLUSTERS" ]; then
-    echo "Found existing PostgreSQL clusters:"
-    echo "$CURRENT_CLUSTERS"
-    echo
+  echo "Found existing PostgreSQL clusters:"
+  echo "$CURRENT_CLUSTERS"
+  echo
 
-    # Stop and remove existing clusters on port 5432
-    for cluster in $CURRENT_CLUSTERS; do
-        version=$(echo $cluster | cut -d'-' -f1)
-        name=$(echo $cluster | cut -d'-' -f2)
+  # Stop and remove existing clusters
+  for cluster in $CURRENT_CLUSTERS; do
+      version=$(echo $cluster | cut -d'-' -f1)
+      name=$(echo $cluster | cut -d'-' -f2)
 
-        # Skip if this is the same version and name we're about to install
-        if [ "$version" = "$NEW_VERSION" ] && [ "$name" = "main" ]; then
-            echo "Skipping cluster $version/$name as it matches the target version and name"
-            continue
-        fi
-        echo "Stopping cluster: $version/$name"
-        sudo pg_ctlcluster $version $name stop || true
+      # Skip if this is the same version and name we're about to install
+      if [ "$version" = "$NEW_VERSION" ] && [ "$name" = "main" ]; then
+          echo "Skipping cluster $version/$name as it matches the target version and name"
+          INSTALL="no"
+          continue
+      fi
 
-        echo "Dropping cluster: $version/$name"
-        sudo pg_dropcluster $version $name || true
-    done
-    echo
+      echo "************************ Removing $version/$name ************************"
+      sudo systemctl stop postgresql@$version-$name || true
+      sudo systemctl disable postgresql@$version-$name || true
+      sudo mv /etc/postgresql/$version /etc/postgresql-$version.bak || true
+      sudo DEBIAN_FRONTEND=noninteractive apt remove --purge postgresql-$version postgresql-client-$version postgresql-contrib-$version -y || true
+      sudo rm -rf /var/lib/postgresql/
+      sudo rm -rf /etc/postgresql/
+  done
 else
   echo "No existing PostgreSQL clusters found on port ${PORT}. Proceeding with installation."
   echo
 
-  sudo DEBIAN_FRONTEND=noninteractive apt install -y postgresql-$NEW_VERSION
+  INSTALL="yes"
+fi
 
+if [ "$INSTALL" = "yes" ]; then
+  sudo DEBIAN_FRONTEND=noninteractive apt install -y postgresql-$NEW_VERSION postgresql-client-$NEW_VERSION postgresql-contrib-$NEW_VERSION
+  sudo DEBIAN_FRONTEND=noninteractive apt install -y postgresql-server-dev-$NEW_VERSION
   sudo systemctl enable postgresql@$NEW_VERSION-main
+else
+  echo "Skipping installation of PostgreSQL ${NEW_VERSION} as it is already installed."
 fi
 
 # Show final status
@@ -62,12 +72,15 @@ sudo pg_lsclusters -h
 sudo systemctl status postgresql@${NEW_VERSION}-main --no-pager
 
 # Stop postgres from running
-sudo systemctl stop postgresql
+sudo systemctl stop postgresql@${NEW_VERSION}-main
 
 # Change where PostgreSQL stores its data
 ORIGINAL_DATA_DIR="/var/lib/postgresql/${NEW_VERSION}/main"
 
 if [ -d "${ORIGINAL_DATA_DIR}" ];then
+  if [ -d "/var/lib/postgresql/${NEW_VERSION}/main.bak" ]; then
+    rm -f -R /var/lib/postgresql/${NEW_VERSION}/main.bak
+  fi
   sudo mv ${ORIGINAL_DATA_DIR} /var/lib/postgresql/${NEW_VERSION}/main.bak
 fi
 
@@ -89,6 +102,23 @@ sudo sed -i "s|port = 5432|port = ${PORT}|g" /etc/postgresql/${NEW_VERSION}/main
 # Allow remote connections
 sudo sed -i "s|#listen_addresses = 'localhost'|listen_addresses = '*'|g" /etc/postgresql/${NEW_VERSION}/main/postgresql.conf
 sudo sed -i "s|host    all             all             127.0.0.1/32            scram-sha-256|host    all             all             0.0.0.0/0            scram-sha-256|g" /etc/postgresql/${NEW_VERSION}/main/pg_hba.conf
+
+# upgrade from previous version if needed
+LAST_CLUSTER=$(echo "$CURRENT_CLUSTERS" | tail -n 1)
+if [ -n "$LAST_CLUSTER" ]; then
+    version=$(echo $LAST_CLUSTER | cut -d'-' -f1)
+    name=$(echo $LAST_CLUSTER | cut -d'-' -f2)
+    if [ "$version" = "$NEW_VERSION" ] && [ "$name" = "main" ]; then
+      echo "...no need to upgrade data, we are on the same cluster version and name"
+    else
+      echo "We need to upgrade the data of the last cluster ${version}-${name}"
+      sudo -u postgres /usr/lib/postgresql/${NEW_VERSION}/bin/pg_upgrade \
+        --old-datadir=/data/postgresql/$version/$name \
+        --new-datadir=/data/postgresql/$NEW_VERSION/main \
+        --old-bindir=/usr/lib/postgresql/$version/bin \
+        --new-bindir=/usr/lib/postgresql/$NEW_VERSION/bin
+    fi
+fi
 
 # Restart PostgreSQL to apply changes
 sudo systemctl start postgresql@${NEW_VERSION}-main
